9e4a30da059c5017a0e01a816ebf3eef1a2695a2
Tosania <1587775166@qq.com>
2026-03-23 22:25:41 +0800

    se

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Scy's blog Ⅲ",
-	subtitle: "Demo Site",
+	title: "Tosania's blog",
+	subtitle: "3",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -54,12 +54,12 @@ export const navBarConfig: NavBarConfig = {
 		},
 		LinkPreset.Archive,
 		{
-			name: "academic",
+			name: "Academic",
 			url: "/academic/",
-			children: [{ name: "publication", url: "/academic/publication/" }],
+			children: [{ name: "Publication", url: "/academic/publication/" }],
 		},
 		{ name: "About", url: "/about/" },
-		{ name: "poems", url: "/poems/" },
+		{ name: "Poems", url: "/poems/" },
 		{
 			name: "GitHub",
 			url: "https://github.com/tosania", // Internal links should not include the base path, as it is automatically added
